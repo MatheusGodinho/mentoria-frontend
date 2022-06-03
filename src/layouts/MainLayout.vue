d6ee46aa18e27a38e6804fb066a68cf6210723e5
MatheusGodinho <matheus.godinho@outlook.com>
2022-06-03 15:03:57 -0300

    feat: initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Mentoria App </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref, watch } from "vue";
+import { useQuasar } from "quasar";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {},
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const $q = useQuasar();
+    $q.dark.set(true);
+    watch(
+      () => $q.dark.isActive,
+      (val) => {
+        console.log(val ? "On dark mode" : "On light mode");
+      }
+    );
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page dark class="flex flex-center column">
+    <img alt="Quasar logo" src="zrp.png" style="width: 400px; height: auto" />
+    <span class="text-h2"> Uma mentoria com Diego e Godinho</span>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
